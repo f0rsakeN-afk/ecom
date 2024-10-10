d6043eefa4098a39ce8d41fe48f666b74fda3f83
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-10 08:20:24 +0545

    Add configuration directory for app settings

---
+++ frontend/src/config/index.js
@@ -0,0 +1,23 @@
+export const registerFormControls = [
+    {
+        name: 'username',
+        label: 'User Name',
+        placeholder: 'Enter your user name',
+        componentType: 'input',
+        type: 'text'
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        placeholder: 'Enter your email address',
+        componentType: 'input',
+        type: 'email'
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Enter your Password',
+        componentType: 'input',
+        type: 'password'
+    }
+]
