592c81aea3b1d9601b6ca25eda4d970187b261ae
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-16 15:52:59 +0545

    Update auth-slice in frontend store

--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
 
 
 const initialState = {
@@ -7,11 +8,31 @@ const initialState = {
     user: null
 }
 
+
+export const registerUser = createAsyncThunk('/auth/register', async (formData) => {
+    const response = await axios.post(`http://localhost:3000/api/v1/users/register`, formData, { withCredentials: true });
+    return response.data;
+})
+
+
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
         setUser: (state, action) => { }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(registerUser.pending, (state) => {
+            state.isLoading = true
+        }).addCase(registerUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.user = null;
+            state.isAuthenticated = false;
+        }).addCase(registerUser.rejected, (state) => {
+            state.isLoading = false;
+            state.user = null;
+            state.isAuthenticated = false;
+        })
     }
 })
 
