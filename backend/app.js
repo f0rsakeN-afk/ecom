b241313bd8d5b01bbcea2b93af94b608fd3a650c
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-16 15:52:30 +0545

    Update express.json middleware and adjust CORS settings in app.js

--- backend/app.js
+++ backend/app.js
@@ -4,20 +4,24 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/userRoutes')
 
-app.use(express.json({ limit: '10kb' }))
+app.use(express.json())
 
-app.use(cors({
-    origin: 'http://localhost:5173/',
-    methods: ['GET', 'POST', 'DELETE', 'PUT'],
-    allowedHeaders: [
-        'Content_type',
-        "Authorization",
-        "Cache-control",
-        "Expires",
-        "Pragma",
-    ],
-    credentials: true
-}))
+
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+        ],
+        credentials: true,
+    })
+);
 
 
 app.use(cookieParser());
