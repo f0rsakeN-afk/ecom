49cdbb67e5ff1d0e1b73c8de736cbea71b0c2691
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-16 08:11:24 +0545

    Implement login page with CommonForm component

--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import CommonForm from "@/components/common/Form";
+import { LoginFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-  return <div>login</div>;
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit() {}
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p className="mt-2">
+          Don't have an account?
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={LoginFormControls}
+        buttonText={"Sign In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 
 export default Login;
