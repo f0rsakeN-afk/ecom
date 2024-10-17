bdbeb5f3464b06696bda26312e155b31ab6ecc0e
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-17 06:01:57 +0545

    Add check for existing user during registration
    - Implement findOne query to check if email already exists in database

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,6 +7,15 @@ const register = async (req, res) => {
     const { userName, email, password } = req.body;
     try {
 
+        const checkUser = await User.findOne({ email })
+        if (checkUser) {
+            return res.json({
+                status: 'failed',
+                message: 'User with that email address already exists'
+            })
+        }
+
+
         const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = new User({
             userName, email, password: hashedPassword
@@ -29,8 +38,38 @@ const register = async (req, res) => {
 
 
 const login = async (req, res) => {
+    const { email, password } = req.body;
     try {
+        const checkUser = await User.findOne({ email })
+        if (!checkUser) {
+            return res.status(400).json({
+                status: 'failed',
+                message: `The user with this email address doesn't exist. Please register first.`
+            })
+        }
+        const checkPasswordMatch = await bcrypt.compare(password, checkUser.password)
 
+        if (!checkPasswordMatch) {
+            return res.status(400).json({
+                status: 'failed',
+                message: 'Incorrect password! Please try again'
+            })
+        }
+
+        const token = jwt.sign({
+            id: checkUser._id, role: checkUser.role, email: checkUser.email
+        }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
+
+
+        res.cookie('token', token, { httpOnly: true, secure: false }).json({
+            status: 'success',
+            message: 'Logged in successfully',
+            user: {
+                email: checkUser.email,
+                role: checkUser.role,
+                id: checkUser._id
+            }
+        })
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -42,4 +81,31 @@ const login = async (req, res) => {
 
 
 
-module.exports = { register }
+const logout = (req, res) => {
+    res.clearCookie('token').json({
+        status: 'success',
+        message: 'Logged out successfully'
+    })
+}
+
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({
+        status: 'failed',
+        message: 'Unauthorized user'
+    })
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next()
+    } catch (error) {
+        res.status(401).json({
+            status: 'failed',
+            message: 'Unauthorized user'
+        })
+    }
+}
+
+module.exports = { register, login, logout, authMiddleware }
