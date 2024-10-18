90c138dc7b5e4389279f55f6c0b093ae87a53f3f
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-18 18:28:51 +0545

    Revise admin Sidebar component

--- frontend/src/components/admin-view/Sidebar.jsx
+++ frontend/src/components/admin-view/Sidebar.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import {
+  ChartNoAxesCombined,
+  BadgeCheck,
+  LayoutDashboard,
+  ShoppingBasket,
+} from "lucide-react";
+import React, { Fragment } from "react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
+import { useNavigate } from "react-router-dom";
 
-const AdminSidebar = () => {
-  return <div>AdminSidebar</div>;
+export const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck />,
+  },
+];
+
+function MenuItems({ setOpen }) {
+  const navigate = useNavigate();
+  return (
+    <nav className="mt-8 flex flex-col gap-2">
+      {adminSidebarMenuItems.map((el) => (
+        <div
+          key={el.id}
+          className="flex items-center gap-2 rounded-md px-3 py-2 cursor-pointer text-muted-foreground hover:bg-muted text-xl"
+          onClick={() => {
+            navigate(`${el.path}`);
+
+            setOpen ? setOpen(false) : null;
+          }}
+        >
+          {el.icon}
+          <span className="">{el.label}</span>
+        </div>
+      ))}
+    </nav>
+  );
+}
+
+const AdminSidebar = ({ open, setOpen }) => {
+  const navigate = useNavigate();
+  return (
+    <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col h-full">
+            <SheetHeader className="border-b">
+              <SheetTitle className="flex gap-1 my-5">
+                <ChartNoAxesCombined size={30} />
+                <span> Admin Panel</span>
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems />
+          </div>
+        </SheetContent>
+      </Sheet>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+        <div
+          className="flex cursor-pointer items-center gap-2"
+          onClick={() => navigate("/admin/dashboard")}
+        >
+          <ChartNoAxesCombined size={32} />
+          <h1 className="text-xl font-extrabold">Admin Panel</h1>
+        </div>
+        <MenuItems setOpen={setOpen} />
+      </aside>
+    </Fragment>
+  );
 };
 
 export default AdminSidebar;
