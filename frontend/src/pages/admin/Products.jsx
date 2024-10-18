8493d8b4b1ccb1146eaf81b7b756cf78b1c3e8dd
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-18 18:29:06 +0545

    Modify admin Products page component

--- frontend/src/pages/admin/Products.jsx
+++ frontend/src/pages/admin/Products.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import CommonForm from "@/components/common/Form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import ProductImageUpload from "@/components/admin-view/imageUpload";
+
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+};
+
+function onSubmit() {}
 
 const AdminProducts = () => {
-  return <div>AdminProducts</div>;
+  const [openCreateDialog, setOpenCreateDialog] = useState(false);
+  const [formData, setFormData] = useState(initialFormData);
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  return (
+    <>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+
+      <Sheet
+        open={openCreateDialog}
+        onOpenChange={() => {
+          setOpenCreateDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <ProductImageUpload
+            file={imageFile}
+            setImageFile={setImageFile}
+            uploadedImageUrl={uploadedImageUrl}
+            setUploadedImageUrl={setUploadedImageUrl}
+          />
+          <div className="py-6">
+            <CommonForm
+              onSubmit={onSubmit}
+              formControls={addProductFormElements}
+              formData={formData}
+              setFormData={setFormData}
+              buttonText={"Add"}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };
 
 export default AdminProducts;
