5804ddb1d6da9a107f2b3674e871e7914de75d85
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-18 18:28:45 +0545

    Update admin Layout component structure

--- frontend/src/components/admin-view/Layout.jsx
+++ frontend/src/components/admin-view/Layout.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./Sidebar";
+import AdminHeader from "./Header";
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className="flex min-h-screen w-full">
+      {/* admin sidebar */}
+      <AdminSidebar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
-        <AdminSidebar />
-        <main className="flex flex-1 bg-muted/40 p-4 md:p-6">
+        {/* admin header */}
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
       </div>
