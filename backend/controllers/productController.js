eba8fc3648c544af10a27fdbf2bc367aae316908
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-24 06:58:44 +0545

    added product controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const { imageUploadUtil } = require("../helpers/cloudinary");
+const Products = require("../models/productModal");
 
 const handleImageUpload = async (req, res) => {
     try {
@@ -17,5 +18,95 @@ const handleImageUpload = async (req, res) => {
     }
 }
 
+const addProduct = async (req, res) => {
+    try {
+        const { image, title, description, category, brand, price, salePrice, totalStock } = req.body;
 
-module.exports = { handleImageUpload };
+        const newProduct = new Products({
+
+            image, title, description, category, brand, price, salePrice, totalStock
+
+        })
+
+        await newProduct.save();
+        res.status(201).json({
+            status: 'success',
+            data: {
+                newProduct
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+const fetchAllProducts = async (req, res) => {
+    try {
+        const products = await Products.find()
+        res.status(200).json({
+            status: 'success',
+            data: {
+                products
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+const editProduct = async (req, res) => {
+    try {
+
+
+        const updatedProduct = await Products.findByIdAndUpdate(req.params.id, req.body, {
+            runValidators: true,
+            new: true
+        })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                updatedProduct
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        await Products.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+
+
+
+
+
+module.exports = { handleImageUpload, addProduct, fetchAllProducts, editProduct, deleteProduct };
