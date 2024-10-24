499c6316bba1f09ac7434d53a28929c28ecc5c17
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-24 06:59:09 +0545

    added product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,17 @@ const express = require('express')
 const router = express.Router();
 
 
-const { handleImageUpload } = require('../controllers/productController')
+const { handleImageUpload, addProduct, editProduct, fetchAllProducts, deleteProduct } = require('../controllers/productController')
 const { upload } = require('../helpers/cloudinary')
 
 
 router.post('/upload-image', upload.single('my_file'), handleImageUpload)
+
+
+router.route('/').get(fetchAllProducts).post(addProduct);
+
+
+router.route('/:id').patch(editProduct).delete(deleteProduct)
 
 
 module.exports = router;
