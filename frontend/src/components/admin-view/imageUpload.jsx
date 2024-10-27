8c093dfea85e626b802c61948283e3fe6e32e843
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-27 07:07:42 +0545

    updated image upload component

--- frontend/src/components/admin-view/imageUpload.jsx
+++ frontend/src/components/admin-view/imageUpload.jsx
@@ -1,55 +1,62 @@
-import React, { useEffect, useRef } from "react";
-import axios from "axios";
+import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
+import { useEffect, useRef } from "react";
 import { Button } from "../ui/button";
+import axios from "axios";
 import { Skeleton } from "../ui/skeleton";
 
-const ProductImageUpload = ({
+function ProductImageUpload({
   imageFile,
   setImageFile,
+  imageLoadingState,
   uploadedImageUrl,
   setUploadedImageUrl,
   setImageLoadingState,
-  imageLoadingState,
-}) => {
+  isEditMode,
+  isCustomStyling = false,
+}) {
   const inputRef = useRef(null);
 
-  const handleImageFileChange = (event) => {
-    //console.log(event.target.files);
+  console.log(isEditMode, "isEditMode");
+
+  function handleImageFileChange(event) {
+    console.log(event.target.files, "event.target.files");
     const selectedFile = event.target.files?.[0];
+    console.log(selectedFile);
+
     if (selectedFile) setImageFile(selectedFile);
-  };
+  }
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const droppedFile = e.dataTransfer.files?.[0];
+  function handleDrop(event) {
+    event.preventDefault();
+    const droppedFile = event.dataTransfer.files?.[0];
     if (droppedFile) setImageFile(droppedFile);
-  };
+  }
 
-  const handleRemoveImage = () => {
+  function handleRemoveImage() {
     setImageFile(null);
     if (inputRef.current) {
       inputRef.current.value = "";
     }
-  };
+  }
 
   async function uploadImageToCloudinary() {
     setImageLoadingState(true);
     const data = new FormData();
     data.append("my_file", imageFile);
-    const res = await axios.post(
+    const response = await axios.post(
       "http://localhost:3000/api/v1/products/upload-image",
       data
     );
-    console.log(res.data);
-    if (res?.data?.success) {
-      setUploadedImageUrl(res.data.url);
+    console.log(response, "response");
+
+    if (response?.data?.status === "success") {
+      setUploadedImageUrl(response.data.result.url);
       setImageLoadingState(false);
     }
   }
@@ -59,12 +66,16 @@ const ProductImageUpload = ({
   }, [imageFile]);
 
   return (
-    <div className="w-full max-w-md mx-auto mt-4">
+    <div
+      className={`w-full  mt-4 ${isCustomStyling ? "" : "max-w-md mx-auto"}`}
+    >
       <Label className="text-lg font-semibold mb-2 block">Upload Image</Label>
       <div
         onDragOver={handleDragOver}
         onDrop={handleDrop}
-        className="border-2 border-dashed rounded-lg p-4 "
+        className={`${
+          isEditMode ? "opacity-60" : ""
+        } border-2 border-dashed rounded-lg p-4`}
       >
         <Input
           id="image-upload"
@@ -72,39 +83,40 @@ const ProductImageUpload = ({
           className="hidden"
           ref={inputRef}
           onChange={handleImageFileChange}
+          disabled={isEditMode}
         />
-
         {!imageFile ? (
           <Label
             htmlFor="image-upload"
-            className="flex flex-col items-center justify-center h-32 cursor-auto"
+            className={`${
+              isEditMode ? "cursor-not-allowed" : ""
+            } flex flex-col items-center justify-center h-32 cursor-pointer`}
           >
-            <UploadCloudIcon className="w-10 h-10 test-muted-foreground mb-2" />
-
-            <span className="">Drag and drop or click to upload image</span>
+            <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
+            <span>Drag & drop or click to upload image</span>
           </Label>
         ) : imageLoadingState ? (
           <Skeleton className="h-10 bg-gray-100" />
         ) : (
           <div className="flex items-center justify-between">
-            <div className="flex items-center ">
-              <FileIcon className="w-8 h-8 mr-2 text-primary" />
+            <div className="flex items-center">
+              <FileIcon className="w-8 text-primary mr-2 h-8" />
             </div>
             <p className="text-sm font-medium">{imageFile.name}</p>
             <Button
               variant="ghost"
               size="icon"
-              className="text-muted-foreground hover:text-foreground "
+              className="text-muted-foreground hover:text-foreground"
               onClick={handleRemoveImage}
             >
               <XIcon className="w-4 h-4" />
-              <span className="sr-only">Remove file</span>
+              <span className="sr-only">Remove File</span>
             </Button>
           </div>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default ProductImageUpload;
