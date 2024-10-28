db40413b8e7a343d69291f8bdc8db11f01cae047
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-28 06:11:25 +0545

    changed cors policy

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(
     cors({
         origin: "http://localhost:5173",
-        methods: ["GET", "POST", "DELETE", "PUT"],
+        methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
         allowedHeaders: [
             "Content-Type",
             "Authorization",
