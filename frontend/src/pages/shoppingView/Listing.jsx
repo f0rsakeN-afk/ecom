86970f3cf82b01a4b9dea306361bb4ef42fdd8e3
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-31 05:34:04 +0545

    layout done for shopping page

--- frontend/src/pages/shoppingView/Listing.jsx
+++ frontend/src/pages/shoppingView/Listing.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import ProductFilter from "@/components/shopping-view/Filter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+import { sortOptions } from "@/config";
 
 const ShoppingListing = () => {
-  return <div>ShoppingListing</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex  items-center justify-between">
+          <h2 className="text-lg font-extrabold ">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">10 Products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDown className="h-4 w-4" />
+                  <span className="">Sort By</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent alien="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                  {sortOptions.map((el) => (
+                    <DropdownMenuRadioItem key={el.id}>
+                      {el.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingListing;
