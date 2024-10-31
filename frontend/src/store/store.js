596008f511670988b10bec311768c5bc36c7e829
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-31 06:26:41 +0545

    added shopProductSlice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './auth-slice'
 import adminProductReducer from './admin/productSlice'
+import shopProductSlice from './shop/productSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        adminProducts: adminProductReducer
+        adminProducts: adminProductReducer,
+        shopProducts: shopProductSlice
     }
 })
 
