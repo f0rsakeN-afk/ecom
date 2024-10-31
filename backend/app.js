fd670d0a5241e0264bd22ae817bcdb7cd7a9c8fe
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-31 05:59:35 +0545

    added shop route

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
+const shopProductRoutes = require('./routes/shopProductRoutes')
 
 app.use(express.json())
 
@@ -30,5 +31,6 @@ app.use(cookieParser());
 
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/shop', shopProductRoutes)
 
 module.exports = app;
