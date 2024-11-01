34b494bf745e4119b06e95262ca9c34f62ca43c5
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-01 19:07:17 +0545

    capitalize

--- frontend/src/components/shopping-view/ProductTiles.jsx
+++ frontend/src/components/shopping-view/ProductTiles.jsx
@@ -22,10 +22,10 @@ const ShoppingProductTile = ({ product }) => {
         <CardContent className="p-4">
           <h2 className="text-xl font-bold mb-2">{product.title}</h2>
           <div className="flex justify-between items-center mb-2">
-            <span className="text-sm  text-muted-foreground">
+            <span className="text-sm capitalize  text-muted-foreground">
               {product.category}
             </span>
-            <span className="text-sm text-muted-foreground">
+            <span className="text-sm capitalize text-muted-foreground">
               {product.brand}
             </span>
           </div>
