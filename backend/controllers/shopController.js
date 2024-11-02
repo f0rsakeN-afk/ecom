02692ea5c5e563155ebbc2b03996250cf6226d79
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-02 06:53:10 +0545

    product details controller done

--- backend/controllers/shopController.js
+++ backend/controllers/shopController.js
@@ -47,4 +47,29 @@ const getFilteredProducts = async (req, res) => {
     }
 }
 
-module.exports = { getFilteredProducts };
+
+const getProductDetails = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Product not found'
+            })
+        }
+        res.status(200).json({
+            status: 'success',
+            data: product
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+
+module.exports = { getFilteredProducts, getProductDetails };
