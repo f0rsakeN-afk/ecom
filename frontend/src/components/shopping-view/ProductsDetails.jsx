d037f81052ad1b552a2b8cccdeb12c461b808fd9
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-03 06:41:01 +0545

    add details page review remain

---
+++ frontend/src/components/shopping-view/ProductsDetails.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Dialog, DialogContent } from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+
+const ProductsDetails = ({ open, setOpen, productDetails }) => {
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]">
+        <div className="relative overflow-hidden rounded-lg">
+          <img
+            src={productDetails?.image}
+            alt={productDetails?.image}
+            width={600}
+            height={600}
+            className="aspect-square w-ull object-cover"
+          />
+        </div>
+        <div className="">
+          <div className="">
+            <h1 className="text-3xl font-extrabold">{productDetails?.title}</h1>
+            <p className="text-muted-foreground text-2xl mb-5 mt-4">
+              {productDetails?.description}
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <p
+              className={`text-3xl font-bold text-primary
+              {productDetails?.price ${
+                productDetails?.salePrice > 0 ? " line-through" : ""
+              }`}
+            >
+              {productDetails?.price}
+            </p>
+
+            {productDetails?.salePrice > 0 ? (
+              <p className="text-2xl font-bold text-muted-foreground">
+                {productDetails?.salePrice}
+              </p>
+            ) : null}
+          </div>
+          <div className="mt-5 mb-5">
+            <Button className="w-full">Add to Cart</Button>
+          </div>
+          <Separator />
+          <div className="maxh-[300px] overflow-auto">
+            <h2 className="text-cl font-bold mb-4">Reviews</h2>
+            <div className="grid gap-6">
+              <div className="flex gap-4">
+                <Avatar className="w-10 h-10 border">
+                  <AvatarFallback>NR</AvatarFallback>
+                </Avatar>
+                <div className="grid gap-1">
+                  <div className="flex items-center gap-2">
+                    <h3 className="font-bold">Lorem, ipsum.</h3>
+                  </div>
+                  <div className="flex items-center gap-0.5"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProductsDetails;
