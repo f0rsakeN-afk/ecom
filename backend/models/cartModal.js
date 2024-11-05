9e5221f9619a4857cdef50206ddf577b0992de9f
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-05 06:35:28 +0545

    add cart Schema

--- backend/models/cartModal.js
+++ backend/models/cartModal.js
@@ -2,8 +2,29 @@ const mongoose = require('mongoose')
 
 
 const cartSchema = new mongoose.Schema({
-
-})
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'A user id is required']
+    },
+    items: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Products',
+                required: [true, 'A product id is required']
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            }
+        }
+    ]
+}, {
+    timestamps: true
+}
+)
 
 const Cart = mongoose.model('cart', cartSchema);
 module.exports = Cart;
