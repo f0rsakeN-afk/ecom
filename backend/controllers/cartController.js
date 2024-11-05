ca94bd2c132b9fda5677700cc245da5bd1ea306c
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-05 07:03:56 +0545

    added fetchCarQuantity controller

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -43,7 +43,45 @@ exports.addToCart = async (req, res) => {
 
 exports.fetchCartItems = async (req, res) => {
     try {
+        const { userId } = req.params
+        if (!userId) {
+            res.status(400).json({
+                status: 'failed',
+                message: 'UserId is required'
+            })
+        }
+        const cart = await Cart.findOne({ userId }).populate({
+            path: 'item.productId',
+            select: 'image titlesalePrice salePrice'
+        })
 
+        if (!cart) {
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Cart not found'
+            })
+        }
+
+        const validItems = cart.items.filter(productItem => productItem.productId);
+        if (validItems.length < cart.items.length) {
+            cart.items = validItems;
+            await cart.save();
+        }
+        const populateCartItems = validItems.map(item => ({
+            productId: item.productId._id,
+            image: item.productId.image,
+            title: item.productId.title,
+            price: item.productId.price,
+            salePrice: item.productId.salePrice,
+            quantity: item.quantity
+        }));
+        res.status(200).json({
+            status: 'success',
+            data: {
+                ...cart._doc,
+                items: populateCartItems
+            }
+        })
     } catch (error) {
         console.log(error)
         res.status(500).json({
