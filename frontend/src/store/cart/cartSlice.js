a08380ef6a74565425cf11bbe0d3caf6e0c1c6fb
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-07 06:48:16 +0545

    added addToCart thunk

---
+++ frontend/src/store/cart/cartSlice.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+
+const initialState = {
+    cartItems: [],
+    isLoading: false
+}
+
+
+export const addToCart = createAsyncThunk('cart/addToCart', async ({ userId, productId, quantity }) => {
+    const response = await axios.post('http://localhost:3000/api/v1/cart/add', { userId, productId, quantity })
+    return response.data;
+})
+
+const shoppingCartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+
+    }
+})
