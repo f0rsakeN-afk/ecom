5e013ede0e4fd2b0a1c98f268eddc830a2f1d650
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-07 06:41:45 +0545

    add cart routes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
 const shopProductRoutes = require('./routes/shopProductRoutes')
+const cartRoutes = require('./routes/cartRoutes')
 
 app.use(express.json())
 
@@ -32,5 +33,6 @@ app.use(cookieParser());
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/shop', shopProductRoutes)
+app.use('/api/v1/cart', cartRoutes)
 
 module.exports = app;
