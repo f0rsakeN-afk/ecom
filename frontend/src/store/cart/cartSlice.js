4e25b3ab267c5e197c3a7b73098f5bb6c5927de5
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-07 07:04:50 +0545

    add builder added

--- frontend/src/store/cart/cartSlice.js
+++ frontend/src/store/cart/cartSlice.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-
-const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
     cartItems: [],
@@ -37,6 +35,41 @@ const shoppingCartSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-
+        builder.addCase(addToCart.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(addToCart.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload.data
+        }).addCase(addToCart.rejected, (state) => {
+            state.isLoading = false;
+            state.cartItems = []
+        }).addCase(fetchCartItems.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(fetchCartItems.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload.data
+        }).addCase(fetchCartItems.rejected, (state) => {
+            state.isLoading = false;
+            state.cartItems = []
+        }).addCase(updateCartQuantity.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(updateCartQuantity.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload.data
+        }).addCase(updateCartQuantity.rejected, (state) => {
+            state.isLoading = false;
+            state.cartItems = []
+        }).addCase(deleteCartItem.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(deleteCartItem.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload.data
+        }).addCase(deleteCartItem.rejected, (state) => {
+            state.isLoading = false;
+            state.cartItems = []
+        })
     }
 })
+
+
+export default shoppingCartSlice.reducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './auth-slice'
 import adminProductReducer from './admin/productSlice'
 import shopProductSlice from './shop/productSlice'
+import shopCartSlice from './cart/cartSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         adminProducts: adminProductReducer,
-        shopProducts: shopProductSlice
+        shopProducts: shopProductSlice,
+        cart: shopCartSlice
     }
 })
 
